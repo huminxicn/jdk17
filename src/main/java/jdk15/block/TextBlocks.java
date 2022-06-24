c8f8c9e32c29e7e3faffc43ec2aacdb509c32572
huminxicn <huminxi2017@gmail.com>
2022-06-24 15:55:06 +0800

    1.调整注释。

--- src/main/java/jdk15/block/TextBlocks.java
+++ src/main/java/jdk15/block/TextBlocks.java
@@ -3,7 +3,7 @@ package jdk15.block;
 /**
  * jdk 15 中添加了文本块（text block），这是从 python 语言借鉴而来的一个新特性。
  * 我们使用三引号来表示包含换行符的文本块。文本块可以让我们更轻松地创建多行文本。
- *
+ * 比如 """你好""" 这种不换行的写法是不支持的。
  * @author huminxi2017@gmail.com
  * @since 2022/05/22 23:23
  */
@@ -13,7 +13,8 @@ public class TextBlocks {
             Yesterday, upon the stair,
             I met a man who wasn't there
             """;
-
+    //不支持的写法。
+    //public static final String NO_SUPPORT = """你好""";
     public static void main(String[] args) {
         System.out.println(OLD.equals(NEW));
     }
